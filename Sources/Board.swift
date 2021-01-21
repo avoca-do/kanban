539aa38d9c60130afa7d92829f93d813d2266180
vaux <zero.griffin@gmail.com>
2021-01-21 14:04:46 +0100

    Add name

--- Sources/Board.swift
+++ Sources/Board.swift
@@ -1,10 +1,12 @@
 import Foundation
 
 public struct Board: Codable, Identifiable {
+    public var name: String
     public var columns = [Column(name: "Do"), .init(name: "Doing"), .init(name: "Done")]
     public let id: String
     
-    public init() {
+    public init(name: String) {
+        self.name = name
         id = UUID().uuidString
     }
 }
