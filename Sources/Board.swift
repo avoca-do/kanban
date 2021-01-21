301f60a19a28c0b3f11d7d59eb4f2cbd43ec8efa
vaux <zero.griffin@gmail.com>
2021-01-21 17:12:28 +0100

    Update column names

--- Sources/Board.swift
+++ Sources/Board.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct Board: Codable, Identifiable {
     public var name: String
-    public var columns = [Column(name: "Do"), .init(name: "Doing"), .init(name: "Done")]
+    public var columns = [Column(name: "DO"), .init(name: "DOING"), .init(name: "DONE")]
     public let id: String
     
     public init(name: String) {
