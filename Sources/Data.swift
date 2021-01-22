81dda6a4535ddcc7927c4018bbf770dd932230cf
vaux <zero.griffin@gmail.com>
2021-01-22 20:38:30 +0100

    Remove bits

--- Sources/Data.swift
+++ Sources/Data.swift
@@ -6,22 +6,6 @@ extension Data {
         append(UInt8())
     }
     
-    var bits: [Bool] {
-        flatMap {
-            $0.bits
-        }
-    }
-    
-    func add(_ bits: [Bool]) -> Self {
-        self + bits.enumerated().reduce(into: [UInt8]()) {
-            if $1.0 % 8 == 0 {
-                $0.append(.init())
-            }
-            guard $1.1 else { return }
-            $0[$0.count - 1] += 1 << ($1.0 % 8)
-        }
-    }
-    
     func add(_ string: String) -> Self {
         self + {
             Swift.withUnsafeBytes(of: UInt16($0.count)) {
