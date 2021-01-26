f21c7bde7be328eb9b17ca1e1b7a8d57e43a525a
vaux <zero.griffin@gmail.com>
2021-01-26 14:07:36 +0100

    Add count to column

--- Sources/Board.Column.swift
+++ Sources/Board.Column.swift
@@ -3,6 +3,11 @@ import Foundation
 public extension Board {
     struct Column: Hashable {
         public internal(set) var title: String
+        
+        public var count: Int {
+            cards.count
+        }
+        
         private var cards: [String]
         
         init() {
