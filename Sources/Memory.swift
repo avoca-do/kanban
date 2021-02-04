fc45c843e1212e51c530ca460101efe5cbcb930b
vaux <zero.griffin@gmail.com>
2021-02-04 12:19:52 +0100

    Update memory

--- Sources/Memory.swift
+++ Sources/Memory.swift
@@ -7,11 +7,12 @@ public final class Memory {
     public let archive = PassthroughSubject<Archive, Never>()
     var subs = Set<AnyCancellable>()
     let save = PassthroughSubject<Archive, Never>()
+    private var first = true
     private let local = PassthroughSubject<Archive, Never>()
     private let remote = PassthroughSubject<Archive, Never>()
     private let pull = PassthroughSubject<Void, Never>()
     private let push = PassthroughSubject<Void, Never>()
-    private let record = PassthroughSubject<CKRecord.ID, Never>()
+    private let record = CurrentValueSubject<CKRecord.ID?, Never>(nil)
     private let queue = DispatchQueue(label: "", qos: .utility)
     
     private var container: CKContainer {
@@ -28,60 +29,60 @@ public final class Memory {
         local
             .removeDuplicates()
             .merge(with: remote.removeDuplicates())
-            .scan(nil) { (previous: Archive?, next: Archive) in
-                print("pre: \(previous?.date) , next: \(next.date)")
-                guard let previous = previous else { return next }
-                return next.date > previous.date ? next : nil
+            .scan(nil) {
+                guard let previous = $0 else { return $1 }
+                return $1.date > previous.date ? $1 : nil
             }
-            .compactMap {
-                print("compact \($0?.date)")
-                return $0
-            }
+            .compactMap { $0 }
             .receive(on: DispatchQueue.main)
-            .sink { [weak self] (archive: Archive) in
-                print("sink \(archive.date)")
-                self?.archive.send(archive)
+            .sink { [weak self] in
+                self?.archive.send($0)
             }
             .store(in: &subs)
         
-        record.combineLatest(pull).sink { [weak self] id, _ in
-            let operation = CKFetchRecordsOperation(recordIDs: [id])
-            operation.configuration.timeoutIntervalForRequest = 15
-            operation.configuration.timeoutIntervalForResource = 20
-            operation.fetchRecordsCompletionBlock = { [weak self] records, _ in
-                guard let value = records?.values.first else { return }
-                self?.remote.send((try! Data(contentsOf: (value["asset"] as! CKAsset).fileURL!)).mutating(transform: Archive.init(data:)))
+        record
+            .compactMap { $0 }
+            .combineLatest(pull)
+            .sink { [weak self] id, _ in
+                let operation = CKFetchRecordsOperation(recordIDs: [id])
+                operation.configuration.timeoutIntervalForRequest = 15
+                operation.configuration.timeoutIntervalForResource = 20
+                operation.fetchRecordsCompletionBlock = { [weak self] records, _ in
+                    guard let value = records?.values.first else { return }
+                    self?.remote.send((try! Data(contentsOf: (value["asset"] as! CKAsset).fileURL!)).mutating(transform: Archive.init(data:)))
+                }
+                self?.container.publicCloudDatabase.add(operation)
             }
-            self?.container.publicCloudDatabase.add(operation)
-        }
-        .store(in: &subs)
+            .store(in: &subs)
         
-        record.combineLatest(push).debounce(for: .seconds(3), scheduler: queue).sink { [weak self] id, _ in
-            let record = CKRecord(recordType: "Archive", recordID: id)
-            record["asset"] = CKAsset(fileURL: FileManager.url)
-            let operation = CKModifyRecordsOperation(recordsToSave: [record])
-            operation.configuration.timeoutIntervalForRequest = 20
-            operation.configuration.timeoutIntervalForResource = 25
-            operation.savePolicy = .allKeys
-            self?.container.publicCloudDatabase.add(operation)
-        }
-        .store(in: &subs)
+        record
+            .compactMap { $0 }
+            .combineLatest(push)
+            .debounce(for: .seconds(1), scheduler: queue)
+            .sink { [weak self] id, _ in
+                let record = CKRecord(recordType: "Archive", recordID: id)
+                record["asset"] = CKAsset(fileURL: FileManager.url)
+                let operation = CKModifyRecordsOperation(recordsToSave: [record])
+                operation.configuration.timeoutIntervalForRequest = 20
+                operation.configuration.timeoutIntervalForResource = 25
+                operation.savePolicy = .allKeys
+                self?.container.publicCloudDatabase.add(operation)
+            }
+            .store(in: &subs)
     }
     
     public func refresh() {
-        FileManager.archive.map(local.send)
-        container.fetchUserRecordID { [weak self] user, _ in
-            user.map {
-                self?.record.send(.init(recordName: "archive_" + $0.recordName))
+        if first {
+            first = false
+            FileManager.archive.map(local.send)
+        }
+        if record.value == nil {
+            container.fetchUserRecordID { [weak self] user, _ in
+                user.map {
+                    self?.record.value = .init(recordName: "archive_" + $0.recordName)
+                }
             }
         }
         pull.send()
-    }
-    
-    private func scan(_ previous: Archive?, _ next: Archive?) -> Archive? {
-        print("pre: \(previous?.date) , next: \(next?.date)")
-        guard let next = next else { return nil }
-        guard let previous = previous else { return next }
-        return next.date > previous.date ? next : nil
     }
 }
