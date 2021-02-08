6c425b2b69fb39ba7155b60a9f3610b6dc4f2a98
vaux <zero.griffin@gmail.com>
2021-02-08 15:14:53 +0100

    Create defaults

---
+++ Sources/Defaults.Key.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public extension Defaults {
+    enum Key: String {
+        case
+        rated,
+        created,
+        spell,
+        correction
+    }
+}
---
+++ Sources/Defaults.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public final class Defaults: UserDefaults {
+    public class var rated: Bool {
+        get { self[.rated] as? Bool ?? false }
+        set { self[.rated] = newValue }
+    }
+    
+    public class var created: Date? {
+        get { self[.created] as? Date }
+        set { self[.created] = newValue }
+    }
+    
+    public class var spell: Bool {
+        get { self[.spell] as? Bool ?? true }
+        set { self[.spell] = newValue }
+    }
+    
+    public class var correction: Bool {
+        get { self[.correction] as? Bool ?? true }
+        set { self[.correction] = newValue }
+    }
+    
+    private class subscript(_ key: Key) -> Any? {
+        get { standard.object(forKey: key.rawValue) }
+        set { standard.setValue(newValue, forKey: key.rawValue) }
+    }
+}
