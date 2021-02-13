5928285d3cf68b1f6f1a984c81ad47fbef77cf3f
vaux <zero.griffin@gmail.com>
2021-02-13 12:35:52 +0100

    Add path accessors

--- Sources/Path.swift
+++ Sources/Path.swift
@@ -24,4 +24,27 @@ public indirect enum Path: Equatable {
         default: return self
         }
     }
+    
+    var board: Int {
+        switch self {
+        case let .board(board): return board
+        case let .column(parent, _), let .card(parent, _): return parent.board
+        default: return 0
+        }
+    }
+    
+    var column: Int {
+        switch self {
+        case let .column(_, column): return column
+        case let .card(parent, _): return parent.column
+        default: return 0
+        }
+    }
+    
+    var card: Int {
+        switch self {
+        case let .card(_, card): return card
+        default: return 0
+        }
+    }
 }
--- Tests/PathTests.swift
+++ Tests/PathTests.swift
@@ -15,4 +15,25 @@ final class PathTests: XCTestCase {
         XCTAssertEqual(.card(.column(.board(1), 2), 3), Path.column(.board(1), 2).down(3))
         XCTAssertEqual(.card(.column(.board(1), 2), 3), Path.card(.column(.board(1), 2), 3))
     }
+    
+    func testBoard() {
+        XCTAssertEqual(0, Path.empty.board)
+        XCTAssertEqual(1, Path.board(1).board)
+        XCTAssertEqual(1, Path.column(.board(1), 2).board)
+        XCTAssertEqual(1, Path.card(.column(.board(1), 2), 3).board)
+    }
+    
+    func testColumn() {
+        XCTAssertEqual(0, Path.empty.column)
+        XCTAssertEqual(0, Path.board(1).column)
+        XCTAssertEqual(2, Path.column(.board(1), 2).column)
+        XCTAssertEqual(2, Path.card(.column(.board(1), 2), 3).column)
+    }
+    
+    func testCard() {
+        XCTAssertEqual(0, Path.empty.card)
+        XCTAssertEqual(0, Path.board(1).card)
+        XCTAssertEqual(0, Path.column(.board(1), 2).card)
+        XCTAssertEqual(3, Path.card(.column(.board(1), 2), 3).card)
+    }
 }
