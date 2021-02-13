4e59c94e0b5896d90c40107f9ec463ce2c4310d0
vaux <zero.griffin@gmail.com>
2021-02-13 11:44:53 +0100

    Make position internal

--- Sources/Position.swift
+++ Sources/Position.swift
@@ -1,11 +1,6 @@
 import Foundation
 
-public struct Position {
-    public let column: Int
-    public let index: Int
-    
-    public init(column: Int, index: Int) {
-        self.column = column
-        self.index = index
-    }
+struct Position {
+    let column: Int
+    let index: Int
 }
