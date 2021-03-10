277ec8f4d44272a5d304b04d5b2f63be41f238d0
vaux <zero.griffin@gmail.com>
2021-03-10 14:59:46 +0100

    Update archive url

--- Sources/Files.swift
+++ Sources/Files.swift
@@ -2,14 +2,15 @@ import Foundation
 
 extension FileManager {
     #if DEBUG
-        static let url = root.appendingPathComponent("archive")
+        static let url = root.appendingPathComponent("avocado.debug.archive")
     #else
-        static let url = root.appendingPathComponent("debug")
+        static let url = root.appendingPathComponent("avocado.archive")
     #endif
     
     static var archive: Archive? {
         get {
             (try? Data(contentsOf: url))?.mutating(transform: Archive.init(data:))
+                ?? (try? Data(contentsOf: root.appendingPathComponent("debug")))?.mutating(transform: Archive.init(data:))
         }
         set {
             try? newValue!.data.write(to: url, options: .atomic)
