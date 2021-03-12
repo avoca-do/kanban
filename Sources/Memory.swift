1bbcefbbaea0736b1a811d4defb1eba75fcd1063
vaux <zero.griffin@gmail.com>
2021-03-12 18:32:58 +0100

    Remove fake error

--- Sources/Memory.swift
+++ Sources/Memory.swift
@@ -105,7 +105,7 @@ public final class Memory {
             .sink { [weak self] id in
                 let subscription = CKQuerySubscription(
                     recordType: Self.type,
-                    predicate: .init(format: "recordI = %@", id),
+                    predicate: .init(format: "recordId = %@", id),
                     options: [.firesOnRecordUpdate])
                 subscription.notificationInfo = .init(alertLocalizationKey: "Avocado")
                 
