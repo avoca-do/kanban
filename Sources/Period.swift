77258e933d94895200886ddb9395174cdab987ba
vaux <zero.griffin@gmail.com>
2021-08-12 15:55:36 +0200

    Update period

--- Sources/Period.swift
+++ Sources/Period.swift
@@ -2,10 +2,7 @@ import Foundation
 
 public enum Period {
     case
-    double,
     year,
-    half,
-    quarter,
     month,
     week,
     day,
@@ -19,14 +16,8 @@ public enum Period {
             return Calendar.current.date(byAdding: .day, value: -7, to: .init())!
         case .month:
             return Calendar.current.date(byAdding: .month, value: -1, to: .init())!
-        case .quarter:
-            return Calendar.current.date(byAdding: .month, value: -3, to: .init())!
-        case .half:
-            return Calendar.current.date(byAdding: .month, value: -6, to: .init())!
         case .year:
             return Calendar.current.date(byAdding: .year, value: -1, to: .init())!
-        case .double:
-            return Calendar.current.date(byAdding: .year, value: -2, to: .init())!
         case let .custom(date):
             return date
         }
